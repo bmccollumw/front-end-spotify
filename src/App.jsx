46dd7c48b574f27442540c619a42961c5d8c4a33
Brian McCollum <bmccollumw@gmail.com>
2025-01-22 21:04:35 -0500

    tested css tailwin - react router next

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function Welcome() {
 
   return (
     <>
-      <h1>Welcome</h1>
+      <h1 className="bg-blue-500 text-white p-4">Welcome</h1>
       <p>Here is a description on the web app and everything that it does.
         I anticipate this being about three sentences at most. 
         I would like there to be some catch that gets people interested in signing in with
